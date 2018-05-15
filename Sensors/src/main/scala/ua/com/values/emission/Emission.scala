001b8d4ebc17cd481429447aadd3ea116f5d24e0
Maria Rabyk <mariia.rabyk@gmail.com>
2018-05-15 18:15:16 +0300

    Edit name of method

--- Sensors/src/main/scala/ua/com/values/emission/Emission.scala
+++ Sensors/src/main/scala/ua/com/values/emission/Emission.scala
@@ -2,8 +2,8 @@ package ua.com.values.emission
 
 import scala.util.Random
 
-object Emission {
-  def getEmission: BigDecimal = {
+object Emission{
+  def getValue: BigDecimal = {
     val randNum = Random.nextInt(9) * 0.001
     BigDecimal.valueOf(randNum).setScale(3)
   }
