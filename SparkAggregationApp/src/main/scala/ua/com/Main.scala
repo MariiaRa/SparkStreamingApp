23d1df045158c6531b4b3d0eeccb813f691ad30b
Maria Rabyk <mariia.rabyk@gmail.com>
2018-06-06 18:46:50 +0300

    Add aggregation

--- SparkAggregationApp/src/main/scala/ua/com/Main.scala
+++ SparkAggregationApp/src/main/scala/ua/com/Main.scala
@@ -3,7 +3,6 @@ package ua.com
 import com.typesafe.config.{Config, ConfigFactory}
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object Main {
 
@@ -19,7 +18,6 @@ object Main {
   val url: String = connection + host + ":" + port + "/" + db
 
   def getSensorData(sparkSession: SparkSession, tableName: String): DataFrame = {
-
     val dictionaryDataDF = sparkSession.read.
       format("jdbc")
       .option("url", url)
@@ -28,7 +26,6 @@ object Main {
       .option("password", password)
       .option("driver", driver)
       .load()
-
     dictionaryDataDF
   }
 
@@ -36,26 +33,44 @@ object Main {
 
     val sparkConf = new SparkConf().setAppName("SparkAggregateStreaming")
       .set("spark.io.compression.codec", "org.apache.spark.io.SnappyCompressionCodec")
-      .set("hive.metastore.uris", "thrift://alpha.gemelen.ent:9083")
-      .set("spark.sql.warehouse.dir", "hdfs://alpha.gemelen.net:8020/user/hive/warehouse")
+      .set("hive.metastore.uris", "thrift://delta.gemelen.net:9083")
+      .set("spark.sql.warehouse.dir", "hdfs://alpha.gemelen.net:8020/apps/hive/warehouse")
 
-    val ssc = new StreamingContext(sparkConf, Seconds(10))
-    ssc.sparkContext.setLogLevel("ERROR")
-
-    val sparkSession = SparkSession.builder
-      .appName("SparkAppStreaming")
+    val spark = SparkSession.builder.config(sparkConf)
+      .appName("SparkAggregation")
+      .enableHiveSupport()
       .getOrCreate()
-    val sensorDictionaryOne = getSensorData(sparkSession, "sensor_data")
-    sensorDictionaryOne.createOrReplaceTempView("dictionaryOne")
 
-    val dictionaryOne = sparkSession.sql("SELECT * FROM dictionaryOne")
+    spark.sparkContext.setLogLevel("ERROR")
+    import spark.sql
+
+    val sensorDictionaryOne = getSensorData(spark, "sensor_data")
+    sensorDictionaryOne.createOrReplaceTempView("dictionaryOne")
+    val dictionaryOne = sql("SELECT * FROM dictionaryOne")
     dictionaryOne.show()
 
-    val sensorDictionaryTwo = getSensorData(sparkSession, "sensor_data")
+    val sensorDictionaryTwo = getSensorData(spark, "sensor_location")
     sensorDictionaryTwo.createOrReplaceTempView("dictionaryTwo")
-
-    val dictionaryTwo = sparkSession.sql("SELECT * FROM dictionaryTwo")
+    val dictionaryTwo = sql("SELECT * FROM dictionaryTwo")
     dictionaryTwo.show()
-  }
 
+    val dfHive = sql("SELECT * FROM sensors")
+    sql("SELECT max(rddID) AS rddId FROM metrics").createOrReplaceTempView("rddId")
+    val rddId1 = sql("SELECT max(rddID) FROM metrics").first().getAs[Long](0)
+
+    sql("SELECT id, max(value) AS max, min(value) AS min, avg(value) AS avg FROM metrics GROUP BY id ORDER BY id").createOrReplaceTempView("stats")
+    sql("SELECT * FROM stats").show()
+    //sql("CREATE TABLE IF NOT EXISTS aggregation (id STRING, type String, location String, max DECIMAL(10,4), min DECIMAL(10,4), avg DECIMAL(10,4)) PARTITIONED BY (rddId BIGINT)")
+
+    val rddId2 = sql("SELECT max(rddID) FROM aggregation").first().getAs[Long](0)
+
+    if (rddId1 > rddId2) {
+      sql("SELECT dictionaryOne.id, dictionaryOne.type, dictionaryTwo.location, stats.max, stats.min, stats.avg" +
+        "FROM dictionaryOne " +
+        "JOIN dictionaryTwo ON (dictionaryOne.id = dictionaryTwo.id) " +
+        "JOIN stats ON (dictionaryOne.id = stats.id) "
+      ).createOrReplaceTempView("aggregated")
+    }
+    sql("SELECT * FROM aggregated").show()
+  }
 }
