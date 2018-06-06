aa735d09062e9e199358863c4bc1986fc45ea021
Maria Rabyk <mariia.rabyk@gmail.com>
2018-06-06 15:14:21 +0300

    Update dependencies according to spark version

--- SparkAppStream/project/Dependencies.scala
+++ SparkAppStream/project/Dependencies.scala
@@ -1,15 +1,13 @@
 import sbt._
 
 object Dependencies {
-  val spartCore = "org.apache.spark" %% "spark-core" % "2.3.0"
-  val sparkSQL = "org.apache.spark" %% "spark-sql" % "2.3.0"
+  val spartCore = "org.apache.spark" %% "spark-core" % "2.0.0"
+  val sparkSQL = "org.apache.spark" %% "spark-sql" % "2.0.0"
   val config = "com.typesafe" % "config" % "1.3.2"
-  val sparkStreaming = "org.apache.spark" %% "spark-streaming" % "2.3.0"
-  val sparkStreamingKafka_010 = "org.apache.spark" % "spark-streaming-kafka-0-10-assembly_2.11" % "2.3.0"
-  val hive = "org.apache.spark" % "spark-hive_2.11" % "2.3.0"
-  val hadoopClient = "org.apache.hadoop" % "hadoop-client" % "2.7.5"
-  val hiveJdbc = "org.apache.hive" % "hive-jdbc" % "2.3.0"
+  val sparkStreaming = "org.apache.spark" %% "spark-streaming" % "2.0.0"
+  val sparkStreamingKafka_010 = "org.apache.spark" %% "spark-streaming-kafka-0-10-assembly" % "2.0.0"
+  val hive = "org.apache.spark" %% "spark-hive" % "2.0.0"
 
   val appDependencies =
-    Seq(spartCore, config, sparkStreaming, sparkStreamingKafka_010, sparkSQL, hive, hadoopClient, hiveJdbc)
+    Seq(spartCore, sparkStreaming, sparkStreamingKafka_010, sparkSQL, hive, config)
 }
