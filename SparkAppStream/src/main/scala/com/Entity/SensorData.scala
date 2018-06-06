c1d6540bfb8ca9f5982d673a06106c74a66b2631
Maria Rabyk <mariia.rabyk@gmail.com>
2018-06-06 15:18:00 +0300

    Add rddId parameter to case class SensorData

--- SparkAppStream/src/main/scala/com/Entity/SensorData.scala
+++ SparkAppStream/src/main/scala/com/Entity/SensorData.scala
@@ -1,3 +1,3 @@
 package com.Entity
 
-case class SensorData (id: String, value: Double, input_time: String, ip: String)
+case class SensorData (id: String, value: Double, input_time: String, ip: String, rddId: Long)
