6e24f98f8077018e37319fbeaf7a91491f90e34b
Maria Rabyk <mariia.rabyk@gmail.com>
2018-06-07 18:55:05 +0300

    Update file

--- Sensors/src/main/scala/ua/com/Main.scala
+++ Sensors/src/main/scala/ua/com/Main.scala
@@ -2,8 +2,9 @@ package ua.com
 
 import akka.actor.{ActorRef, ActorSystem}
 import akka.stream.ActorMaterializer
+import ua.com.sensors.Sensor
 import ua.com.sensors.TemperatureSensor.Start
-import ua.com.sensors.{EmissionSensor, HumiditySensor, TemperatureSensor}
+import ua.com.values.{Emission, Humidity, Parameters, Temperature}
 
 import scala.concurrent.ExecutionContextExecutor
 
@@ -13,24 +14,36 @@ object Main extends App {
 
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+  val params = Map("T" -> Temperature, "E" -> Emission, "H" -> Humidity)
 
-  val device1: ActorRef = system.actorOf(TemperatureSensor.props("T1"), "TemperatureActor")
-  val device2: ActorRef = system.actorOf(TemperatureSensor.props("T2"), "TemperatureActor")
-  val device3: ActorRef = system.actorOf(TemperatureSensor.props("T3"), "TemperatureActor")
-  val device4: ActorRef = system.actorOf(HumiditySensor.props("H1"), "HumidityActor")
-  val device5: ActorRef = system.actorOf(HumiditySensor.props("H2"), "HumidityActor")
-  val device6: ActorRef = system.actorOf(HumiditySensor.props("H3"), "HumidityActor")
-  val device7: ActorRef = system.actorOf(EmissionSensor.props("E1"), "EmissionActor")
-  val device8: ActorRef = system.actorOf(EmissionSensor.props("E2"), "EmissionActor")
-  val device9: ActorRef = system.actorOf(EmissionSensor.props("E3"), "EmissionActor")
+  private def createActor(deviceID: String, value: Parameters): ActorRef = {
+    system.actorOf(Sensor.props(deviceID, value), "Actor" + deviceID)
+  }
 
-  device9 ! Start
-  device4 ! Start
-  device6 ! Start
-  device8 ! Start
-  device5 ! Start
-  device2 ! Start
-  device3 ! Start
-  device7 ! Start
-  device1 ! Start
+  override def main(args: Array[String]): Unit = {
+
+    params.foreach { case (key, value) => createActor(key, value) ! Start }
+
+  }
+
+
+  /*  val device1: ActorRef = system.actorOf(TemperatureSensor.props("T1"), "TemperatureActor")
+    val device2: ActorRef = system.actorOf(TemperatureSensor.props("T2"), "TemperatureActor")
+    val device3: ActorRef = system.actorOf(TemperatureSensor.props("T3"), "TemperatureActor")
+    val device4: ActorRef = system.actorOf(HumiditySensor.props("H1"), "HumidityActor")
+    val device5: ActorRef = system.actorOf(HumiditySensor.props("H2"), "HumidityActor")
+    val device6: ActorRef = system.actorOf(HumiditySensor.props("H3"), "HumidityActor")
+    val device7: ActorRef = system.actorOf(EmissionSensor.props("E1"), "EmissionActor")
+    val device8: ActorRef = system.actorOf(EmissionSensor.props("E2"), "EmissionActor")
+    val device9: ActorRef = system.actorOf(EmissionSensor.props("E3"), "EmissionActor")
+
+    device9 ! Start
+    device4 ! Start
+    device6 ! Start
+    device8 ! Start
+    device5 ! Start
+    device2 ! Start
+    device3 ! Start
+    device7 ! Start
+    device1 ! Start*/
 }
