f62d7af3640a7086a9c78eda8584b2df2a5e93a5
Maria Rabyk <mariia.rabyk@gmail.com>
2018-06-08 11:14:15 +0300

    Change the logic of actor creation

--- Sensors/src/main/scala/ua/com/Main.scala
+++ Sensors/src/main/scala/ua/com/Main.scala
@@ -2,48 +2,42 @@ package ua.com
 
 import akka.actor.{ActorRef, ActorSystem}
 import akka.stream.ActorMaterializer
+import com.typesafe.config.{Config, ConfigFactory}
+import org.slf4j.LoggerFactory
 import ua.com.sensors.Sensor
-import ua.com.sensors.TemperatureSensor.Start
+import ua.com.sensors.Sensor.Start
 import ua.com.values.{Emission, Humidity, Parameters, Temperature}
 
 import scala.concurrent.ExecutionContextExecutor
 
-object Main extends App {
+object Main {
 
   implicit def system: ActorSystem = ActorSystem("ActorSystem")
 
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
-  val params = Map("T" -> Temperature, "E" -> Emission, "H" -> Humidity)
 
-  private def createActor(deviceID: String, value: Parameters): ActorRef = {
-    system.actorOf(Sensor.props(deviceID, value), "Actor" + deviceID)
+  private val logger = LoggerFactory.getLogger(this.getClass)
+
+  val params: Map[String, Parameters] = Map("T" -> Temperature, "E" -> Emission, "H" -> Humidity)
+
+  val myConf: Config = ConfigFactory.load()
+  val number: Int = myConf.getInt("actor.number")
+
+  private def startActor(deviceID: String, value: Parameters, n: Int): Unit = {
+    for (i <- 1 to 3) {
+      logger.info("Actor has been created.")
+      val device: ActorRef = system.actorOf(Sensor.props(deviceID, value), deviceID)
+      device ! Start
+    }
   }
 
-  override def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {
 
-    params.foreach { case (key, value) => createActor(key, value) ! Start }
-
+    params.foreach {
+      case (key, value) =>
+        startActor(key, value, number)
+    }
   }
 
-
-  /*  val device1: ActorRef = system.actorOf(TemperatureSensor.props("T1"), "TemperatureActor")
-    val device2: ActorRef = system.actorOf(TemperatureSensor.props("T2"), "TemperatureActor")
-    val device3: ActorRef = system.actorOf(TemperatureSensor.props("T3"), "TemperatureActor")
-    val device4: ActorRef = system.actorOf(HumiditySensor.props("H1"), "HumidityActor")
-    val device5: ActorRef = system.actorOf(HumiditySensor.props("H2"), "HumidityActor")
-    val device6: ActorRef = system.actorOf(HumiditySensor.props("H3"), "HumidityActor")
-    val device7: ActorRef = system.actorOf(EmissionSensor.props("E1"), "EmissionActor")
-    val device8: ActorRef = system.actorOf(EmissionSensor.props("E2"), "EmissionActor")
-    val device9: ActorRef = system.actorOf(EmissionSensor.props("E3"), "EmissionActor")
-
-    device9 ! Start
-    device4 ! Start
-    device6 ! Start
-    device8 ! Start
-    device5 ! Start
-    device2 ! Start
-    device3 ! Start
-    device7 ! Start
-    device1 ! Start*/
 }
