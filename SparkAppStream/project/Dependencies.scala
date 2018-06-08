0194ce350d6197d6d6b0f38a12ccbfb92e3e288f
Maria Rabyk <mariia.rabyk@gmail.com>
2018-06-08 13:39:02 +0300

    Edit build.sbt file

--- SparkAppStream/project/Dependencies.scala
+++ SparkAppStream/project/Dependencies.scala
@@ -1,12 +1,15 @@
 import sbt._
 
+val sparkVersion = "2.0.0"
+val configVersion = "1.3.2"
+
 object Dependencies {
-  val spartCore = "org.apache.spark" %% "spark-core" % "2.0.0"
-  val sparkSQL = "org.apache.spark" %% "spark-sql" % "2.0.0"
-  val config = "com.typesafe" % "config" % "1.3.2"
-  val sparkStreaming = "org.apache.spark" %% "spark-streaming" % "2.0.0"
-  val sparkStreamingKafka_010 = "org.apache.spark" %% "spark-streaming-kafka-0-10-assembly" % "2.0.0"
-  val hive = "org.apache.spark" %% "spark-hive" % "2.0.0"
+  val spartCore = "org.apache.spark" %% "spark-core" % sparkVersion
+  val sparkSQL = "org.apache.spark" %% "spark-sql" % sparkVersion
+  val config = "com.typesafe" % "config" % configVersion
+  val sparkStreaming = "org.apache.spark" %% "spark-streaming" % sparkVersion
+  val sparkStreamingKafka_010 = "org.apache.spark" %% "spark-streaming-kafka-0-10-assembly" % sparkVersion
+  val hive = "org.apache.spark" %% "spark-hive" % sparkVersion
 
   val appDependencies =
     Seq(spartCore, sparkStreaming, sparkStreamingKafka_010, sparkSQL, hive, config)
