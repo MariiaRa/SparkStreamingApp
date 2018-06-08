3c8c08b5a9fd61876ca52d7dc54be6c56eba4590
Maria Rabyk <mariia.rabyk@gmail.com>
2018-06-08 17:10:49 +0300

    Add versions for dependencies

--- SparkAggregationApp/project/Dependencies.scala
+++ SparkAggregationApp/project/Dependencies.scala
@@ -1,15 +1,19 @@
 import sbt._
 
 object Dependencies {
-  val spartCore = "org.apache.spark" %% "spark-core" % "2.0.0"
-  val sparkSQL = "org.apache.spark" %% "spark-sql" % "2.0.0"
+  val sparkVersion = "2.0.0"
+  val configVersion = "1.3.2"
+  val hiveJdbcVersion = "1.2.1"
+  val postgreSQLVersion = "42.2.2"
+
+  val spartCore = "org.apache.spark" %% "spark-core" % sparkVersion
+  val sparkSQL = "org.apache.spark" %% "spark-sql" % sparkVersion
   val config = "com.typesafe" % "config" % "1.3.2"
-  val sparkStreaming = "org.apache.spark" %% "spark-streaming" % "2.0.0"
-  val hive = "org.apache.spark" %% "spark-hive" % "2.0.0"
-  val hadoopClient = "org.apache.hadoop" % "hadoop-client" % "2.7.1"
-  val hiveJdbc = "org.apache.hive" % "hive-jdbc" % "1.2.1"
-  val postgreSQL = "org.postgresql" % "postgresql" % "42.2.2"
+  val sparkStreaming = "org.apache.spark" %% "spark-streaming" % sparkVersion
+  val hive = "org.apache.spark" %% "spark-hive" % sparkVersion
+  val hiveJdbc = "org.apache.hive" % "hive-jdbc" % hiveJdbcVersion
+  val postgreSQL = "org.postgresql" % "postgresql" % postgreSQLVersion
 
   val appDependencies =
-    Seq(spartCore, config, sparkStreaming, sparkSQL, hive, hadoopClient, hiveJdbc, postgreSQL)
+    Seq(spartCore, config, sparkStreaming, sparkSQL, hive, hiveJdbc, postgreSQL)
 }
