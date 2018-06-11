1d3fde4268741da1744c3ba0e3d6c16216952175
Maria Rabyk <mariia.rabyk@gmail.com>
2018-06-11 11:29:07 +0300

    Edit mistake

--- SparkAppStream/project/Dependencies.scala
+++ SparkAppStream/project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   val sparkVersion = "2.0.0"
   val configVersion = "1.3.2"
 
-  val spartCore = "org.apache.spark" %% "spark-core" % sparkVersion
+  val sparkCore = "org.apache.spark" %% "spark-core" % sparkVersion
   val sparkSQL = "org.apache.spark" %% "spark-sql" % sparkVersion
   val config = "com.typesafe" % "config" % configVersion
   val sparkStreaming = "org.apache.spark" %% "spark-streaming" % sparkVersion
@@ -12,5 +12,5 @@ object Dependencies {
   val hive = "org.apache.spark" %% "spark-hive" % sparkVersion
 
   val appDependencies =
-    Seq(spartCore, sparkStreaming, sparkStreamingKafka_010, sparkSQL, hive, config)
+    Seq(sparkCore, sparkStreaming, sparkStreamingKafka_010, sparkSQL, hive, config)
 }
