c2f9bae2f6af233db72d6118bb487d62d8d32563
maria ra <mariia.rabyk@gmail.com>
2018-09-24 17:57:02 +0300

    Edited path in getBucketPath method

--- FlinkStreaming/src/main/scala/com/ua/Bucketing/MyBucketer.scala
+++ FlinkStreaming/src/main/scala/com/ua/Bucketing/MyBucketer.scala
@@ -4,9 +4,17 @@ import org.apache.flink.streaming.connectors.fs.Clock
 import org.apache.flink.streaming.connectors.fs.bucketing.Bucketer
 import org.apache.hadoop.fs.Path
 
-class MyBucketer(elem: String) extends Bucketer[String] {
+class MyBucketer extends Bucketer[String] {
 
- override def getBucketPath(clock: Clock, basePath: Path, element: String): Path = {
-new Path(basePath + "/" + element.split(",")(4))
- }
+  override def getBucketPath(clock: Clock, basePath: Path, element: String): Path = {
+
+    val formatDate = new java.text.SimpleDateFormat("yyyy-MM-dd-HH-mm")
+    val date = formatDate.format(new java.util.Date())
+
+    new Path(basePath + "/" + date + "/" + element.split(",")(4))
+  }
 }
+
+
+
+
