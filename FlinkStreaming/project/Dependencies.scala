1c383c463405d4b1b3e04af7e54cc7d0e939f268
maria ra <mariia.rabyk@gmail.com>
2018-09-24 17:58:00 +0300

    Edited dependency versions

--- FlinkStreaming/project/Dependencies.scala
+++ FlinkStreaming/project/Dependencies.scala
@@ -2,21 +2,32 @@ import sbt._
 
 object Dependencies {
   val configVersion = "1.3.2"
-  val flinkVersion = "1.5.0"
+  val flinkVersion = "1.6.1"
 
   val config = "com.typesafe" % "config" % configVersion
-  val flink = "org.apache.flink" %% "flink-scala" % flinkVersion
-  val flinkSteaming = "org.apache.flink" %% "flink-streaming-scala" % flinkVersion
-  val flinkClients = "org.apache.flink" %% "flink-clients" % flinkVersion
-  val flinkKafkaConnector = "org.apache.flink" %% "flink-connector-kafka-0.11" % flinkVersion
-  val flinkTable = "org.apache.flink" %% "flink-table" % flinkVersion
-  val flinkCore = "org.apache.flink" % "flink-core" % flinkVersion
-  val flinkHdfsConnector = "org.apache.flink" %% "flink-connector-filesystem" % flinkVersion
-  val flinkJdbc = "org.apache.flink" % "flink-jdbc" % "1.5.0"
-  val hadoop = "org.apache.hadoop" % "hadoop-common" % "2.7.1"
+  val flinkSteaming = "org.apache.flink" %% "flink-streaming-scala" % "1.6.1"
+  val flinkClients = "org.apache.flink" %% "flink-clients" % "1.6.1"
+  val flinkKafkaConnector = "org.apache.flink" %% "flink-connector-kafka-0.11" % "1.6.1"
+  val flinkTable = "org.apache.flink" %% "flink-table" % "1.6.1"
+  val flinkCore = "org.apache.flink" % "flink-core" % "1.6.1"
+  val flinkHdfsConnector = "org.apache.flink" %% "flink-connector-filesystem" % "1.6.1"
+  val flinkJdbc = "org.apache.flink" % "flink-jdbc" % "1.6.1"
+  val hadoop = "org.apache.hadoop" % "hadoop-common" % "2.7.5"
   val driver = "org.postgresql" % "postgresql" % "42.2.2"
-
+  val avro = "org.apache.avro" % "avro" % "1.8.2"
+  val hdfs = "org.apache.hadoop" % "hadoop-hdfs" % "2.7.5"
   val appDependencies =
-    Seq(config, flink, flinkSteaming, flinkClients, flinkKafkaConnector
-      , flinkTable, flinkCore, flinkJdbc, flinkHdfsConnector, hadoop, driver)
+    Seq(config,
+      flinkSteaming,
+      flinkClients,
+      flinkKafkaConnector,
+      flinkTable,
+      flinkCore,
+      flinkJdbc,
+      flinkHdfsConnector,
+      hadoop,
+      hdfs,
+      avro
+      //driver
+    )
 }
