9f4ac66eac951a52b24b8b46ae50bfea5200e670
maria ra <mariia.rabyk@gmail.com>
2018-09-24 17:57:24 +0300

    Added repos

--- FlinkStreaming/project/DefaultResolversPlugin.scala
+++ FlinkStreaming/project/DefaultResolversPlugin.scala
@@ -2,13 +2,18 @@ import sbt.Keys._
 import sbt._
 
 object DefaultResolversPlugin extends AutoPlugin {
-
-  val apache = "apache-snapshots" at "http://repository.apache.org/snapshots/"
-
+  val apache = "Apache Development Snapshot Repository" at "https://repository.apache.org/content/repositories/snapshots/"
+  val apacheSnapshots = "apache-snapshots" at "http://repository.apache.org/snapshots/"
+  val apacheRepository = "Apache Repository" at "https://repository.apache.org/content/repositories/releases/"
   val typeSafeResolver = "Typesafe repository releases" at "http://repo.typesafe.com/typesafe/releases/"
-
+  val confluent = "Confluent" at "http://packages.confluent.io/maven"
+  val sonatype = "Sonatype" at "https://oss.sonatype.org/content/repositories/releases/"
   override lazy val projectSettings = Seq(
     resolvers ++= Seq(
-      apache)
+      apache,
+      apacheSnapshots,
+      apacheRepository,
+      typeSafeResolver,
+      sonatype)
   )
 }
