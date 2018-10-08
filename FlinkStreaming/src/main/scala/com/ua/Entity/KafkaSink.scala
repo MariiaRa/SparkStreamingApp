92ae5081ed9feb183f4278ec5449b60502a11c3c
maria ra <mariia.rabyk@gmail.com>
2018-10-08 10:53:32 +0300

    Edited kafka producer

--- FlinkStreaming/src/main/scala/com/ua/Entity/KafkaSink.scala
+++ FlinkStreaming/src/main/scala/com/ua/Entity/KafkaSink.scala
@@ -2,38 +2,10 @@ package com.ua.Entity
 
 import com.typesafe.config.{Config, ConfigFactory}
 import com.ua.Serializer.AvroObjectSerializer
-import org.apache.avro.Schema
-import org.apache.avro.Schema.Parser
 import org.apache.avro.generic.GenericRecord
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer010
 
 object KafkaSink {
-
-  val str =
-    """{
-          "namespace": "kakfa-avro.test",
-          "type": "record",
-          "name": "sensor",
-          "fields":[
-            {
-              "name": "id", "type": "string"
-            },
-            {
-              "name": "value",  "type": "double"
-            },
-            {
-              "name": "input_time", "type": "string"
-            },
-            {
-              "name": "ip", "type": "string"
-            },
-            {
-              "name": "time_bucket", "type": "long"
-            }
-          ]
-        }"""
-
-  lazy val schema: Schema = new Parser().parse(str)
 
   val myConf: Config = ConfigFactory.load()
   val topic: String = myConf.getString("kafka-remote.toTopic")
@@ -41,7 +13,7 @@ object KafkaSink {
   val myProducer = new FlinkKafkaProducer010[GenericRecord](
     brokers, // broker list
     topic, // target topic
-    new AvroObjectSerializer(schema)) // serialization schema
+    new AvroObjectSerializer()) // serialization schema
 
   myProducer.setWriteTimestampToKafka(true)
 }
