6e9b7cd0b929d2722ed08122d6b2977d427dcced
maria ra <mariia.rabyk@gmail.com>
2018-10-08 10:13:39 +0300

    Edited versions of dependencies

--- FlinkStreaming/project/Dependencies.scala
+++ FlinkStreaming/project/Dependencies.scala
@@ -7,15 +7,17 @@ object Dependencies {
   val config = "com.typesafe" % "config" % configVersion
   val flinkSteaming = "org.apache.flink" %% "flink-streaming-scala" % "1.6.1"
   val flinkClients = "org.apache.flink" %% "flink-clients" % "1.6.1"
-  val flinkKafkaConnector = "org.apache.flink" %% "flink-connector-kafka-0.11" % "1.6.1"
+  val flinkKafkaConnector = "org.apache.flink" %% "flink-connector-kafka-0.10" % "1.6.1"
   val flinkTable = "org.apache.flink" %% "flink-table" % "1.6.1"
   val flinkCore = "org.apache.flink" % "flink-core" % "1.6.1"
   val flinkHdfsConnector = "org.apache.flink" %% "flink-connector-filesystem" % "1.6.1"
   val flinkJdbc = "org.apache.flink" % "flink-jdbc" % "1.6.1"
-  val hadoop = "org.apache.hadoop" % "hadoop-common" % "2.7.5"
+  val hadoop = "org.apache.hadoop" % "hadoop-common" % "2.7.3"
   val driver = "org.postgresql" % "postgresql" % "42.2.2"
   val avro = "org.apache.avro" % "avro" % "1.8.2"
-  val hdfs = "org.apache.hadoop" % "hadoop-hdfs" % "2.7.5"
+  val hdfs = "org.apache.hadoop" % "hadoop-hdfs" % "2.7.3"
+  val linkAvro = "org.apache.flink" % "flink-avro" % "1.6.1"
+
   val appDependencies =
     Seq(config,
       flinkSteaming,
@@ -27,7 +29,7 @@ object Dependencies {
       flinkHdfsConnector,
       hadoop,
       hdfs,
-      avro
-      //driver
+      avro,
+      linkAvro
     )
 }
